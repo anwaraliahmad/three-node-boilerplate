a81bb857ad6810294f58e12601a1dd6157274828
Anwar Ali-Ahmad <anwar.aliahmad@icloud.com>
2016-08-20 21:11:55 -0700

    Set up basic scene

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,33 @@
 window.THREE = require('three');
 window.OrbitControls = require('three-orbit-controls')(THREE);
-window.DAT = require('dat-gui');
+window.stats = require('stats.js');
 
+// The setting up the scene, which contains all objects
 let scene = new THREE.Scene();
+
 // Camera with 45 deg FoV and aspect ratio of window's innerWidth/innerHeight, render distance interval [1,5000]
 let camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 5000);
 camera.position.z = 30;
+
+// Setting up orbit controlling of the camera
+let controls = new OrbitControls(camera);
+
 //giving the document a WebGL renderer
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
-renderer.setClearColor(0xffffff, .5);
+
+// Adding a visualization of the x, y, and z axes
+let axisHelper = new THREE.AxisHelper(4);
+scene.add(axisHelper);
 
 function render () {
-
+  controls.update();
+  // Rendering the scene
+  renderer.render(scene, camera);
+  // Telling the browser that the function render seeks to reanimate frame
+  window.requestAnimationFrame(render);
 }
 
-
-
-
+// Initial call of render()
 requestAnimationFrame(render);
